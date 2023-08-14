5029818522d1a85208b3dc16b9d819d6759b1a39
Vyacheslav <vyacheslav.tomasevich@gmail.com>
2023-08-14 07:11:09 +0000

    сконфигурирована базовая настройка typescript

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TinkoffInvestAPI = void 0;
+function TinkoffInvestAPI() {
+    return 'work';
+}
+exports.TinkoffInvestAPI = TinkoffInvestAPI;
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export function TinkoffInvestAPI() {
+  return 'work'
+}
