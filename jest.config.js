1265f00c7e5ee57b4c14f52e8c1b4864f538d00f
Vyacheslav <vyacheslav.tomasevich@gmail.com>
2023-08-17 16:27:30 +0000

    выставлено ограничение по конкретному тесту я знаю, можно отдельно тестировать, но мне лень вспоминать команды

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   transform: { '^.+\\.ts?$': 'ts-jest' },
   testEnvironment: 'node',
-  testRegex: '/test/.*\\.(test|spec)?\\.(ts)$',
+  testRegex: '/test/InstrumentsService/Bonds.test.ts',
+  // testRegex: '/test/.*\\.(test|spec)?\\.(ts)$',
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
   verbose: true
 }
