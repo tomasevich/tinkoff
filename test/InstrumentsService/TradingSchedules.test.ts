b32e93693c8be10ca59dc0193413cd9a38d899bd
Vyacheslav <vyacheslav.tomasevich@gmail.com>
2023-08-18 09:57:29 +0300

    упрощен тест проверки расписаний торгов

--- test/InstrumentsService/TradingSchedules.test.ts
+++ test/InstrumentsService/TradingSchedules.test.ts
@@ -1,76 +1,38 @@
 import dotenv from 'dotenv'
 
-import { InstrumentsService, TradingSchedulesResponse } from '../../src'
+import { InstrumentsService } from '../../src'
 
 dotenv.config({ path: './.env.test' })
 
 const TOKEN = process.env.TINKOFF_INVEST_API_TOKEN ?? ''
 const instrumentsService = new InstrumentsService(TOKEN, true)
 
-describe('InstrumentsService', () => {
-  describe('TradingSchedules', () => {
-    describe('Try `exchange` as `MOEX_MORNING`', () => {
-      let response: TradingSchedulesResponse
-
-      beforeAll(async () => {
-        const currTime = new Date()
-        const nextTime = new Date(currTime)
-        nextTime.setDate(currTime.getDate() + 1)
-
-        response = await instrumentsService.TradingSchedules({
-          from: currTime.toISOString(),
-          to: nextTime.toISOString(),
-          exchange: 'MOEX_MORNING'
-        })
-      })
-
-      test('Expect `response` to have property `exchanges`', async () => {
-        expect(response).toHaveProperty('exchanges')
-      })
-
-      test('Expect `exchanges[0]` to have properties `exchange` & `days`', async () => {
-        expect(response.exchanges[0]).toHaveProperty('exchange')
-        expect(response.exchanges[0]).toHaveProperty('days')
-      })
-
-      test('Expect `exchanges[0].exchange` to equal `MOEX_MORNING`', async () => {
-        expect(response.exchanges[0].exchange).toEqual('MOEX_MORNING')
-      })
-
-      test('Expect `exchanges[0].days[0]` to have property `date` & `isTradingDay`', async () => {
-        expect(response.exchanges[0].days[0]).toHaveProperty('date')
-        expect(response.exchanges[0].days[0]).toHaveProperty('isTradingDay')
-      })
+describe('exchange as "MOEX_MORNING"', () => {
+  test('Expect "response" to have property "exchanges"', async () => {
+    const currTime = new Date()
+    const nextTime = new Date(currTime)
+    nextTime.setDate(currTime.getDate() + 1)
+    
+    const response = await instrumentsService.TradingSchedules({
+      from: currTime.toISOString(),
+      to: nextTime.toISOString(),
+      exchange: 'MOEX_MORNING'
     })
+    expect(response).toHaveProperty('exchanges')
+  })
+})
 
-    describe('Try `exchange` as ``', () => {
-      let response: TradingSchedulesResponse
-
-      beforeAll(async () => {
-        const currTime = new Date()
-        const nextTime = new Date(currTime)
-        nextTime.setDate(currTime.getDate() + 1)
-
-        response = await instrumentsService.TradingSchedules({
-          from: currTime.toISOString(),
-          to: nextTime.toISOString(),
-          exchange: 'MOEX_MORNING'
-        })
-      })
-
-      test('Expect `response` to have property `exchanges`', async () => {
-        expect(response).toHaveProperty('exchanges')
-      })
-
-      test('Expect `exchanges[0]` to have properties `exchange` & `days`', async () => {
-        expect(response.exchanges[0]).toHaveProperty('exchange')
-        expect(response.exchanges[0]).toHaveProperty('days')
-      })
-
-      test('Expect `exchanges[0].days[0]` to have property `date` & `isTradingDay`', async () => {
-        expect(response.exchanges[0].days[0]).toHaveProperty('date')
-        expect(response.exchanges[0].days[0]).toHaveProperty('isTradingDay')
-      })
+describe('exchange as ""', () => {
+  test('Expect "response" to have property "exchanges"', async () => {
+    const currTime = new Date()
+    const nextTime = new Date(currTime)
+    nextTime.setDate(currTime.getDate() + 1)
+    
+    const response = await instrumentsService.TradingSchedules({
+      from: currTime.toISOString(),
+      to: nextTime.toISOString(),
+      exchange: ''
     })
+    expect(response).toHaveProperty('exchanges')
   })
 })
