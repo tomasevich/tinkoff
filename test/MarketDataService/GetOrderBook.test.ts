85bc65d142d57b0db12801dc951b969deba63de4
Vyacheslav <vyacheslav.tomasevich@gmail.com>
2023-08-18 21:05:07 +0000

    добавлено сообщение о запуске тестов в рабочее время

--- test/MarketDataService/GetOrderBook.test.ts
+++ test/MarketDataService/GetOrderBook.test.ts
@@ -7,25 +7,27 @@ dotenv.config({ path: './.env.test' })
 const TOKEN = process.env.TINKOFF_INVEST_API_TOKEN ?? ''
 const marketDataService = new MarketDataService(TOKEN, true)
 
-describe('depth as "10"', () => {
-  describe('instrumentId as "6afa6f80-03a7-4d83-9cf0-c19d7d021f76"', () => {
-    test('Expect "response" to have "all" properties', async () => {
-      const response = await marketDataService.GetOrderBook({
-        depth: 10,
-        instrumentId: '6afa6f80-03a7-4d83-9cf0-c19d7d021f76'
+describe('ONLY ON WORKING TIME', () => {
+  describe('depth as "10"', () => {
+    describe('instrumentId as "6afa6f80-03a7-4d83-9cf0-c19d7d021f76"', () => {
+      test('Expect "response" to have "all" properties', async () => {
+        const response = await marketDataService.GetOrderBook({
+          depth: 10,
+          instrumentId: '6afa6f80-03a7-4d83-9cf0-c19d7d021f76'
+        })
+        expect(response).toHaveProperty('figi')
+        expect(response).toHaveProperty('depth')
+        expect(response).toHaveProperty('bids')
+        expect(response).toHaveProperty('asks')
+        expect(response).toHaveProperty('lastPrice')
+        expect(response).toHaveProperty('closePrice')
+        expect(response).toHaveProperty('limitUp')
+        expect(response).toHaveProperty('limitDown')
+        expect(response).toHaveProperty('lastPriceTs')
+        expect(response).toHaveProperty('closePriceTs')
+        expect(response).toHaveProperty('orderbookTs')
+        expect(response).toHaveProperty('instrumentUid')
       })
-      expect(response).toHaveProperty('figi')
-      expect(response).toHaveProperty('depth')
-      expect(response).toHaveProperty('bids')
-      expect(response).toHaveProperty('asks')
-      expect(response).toHaveProperty('lastPrice')
-      expect(response).toHaveProperty('closePrice')
-      expect(response).toHaveProperty('limitUp')
-      expect(response).toHaveProperty('limitDown')
-      expect(response).toHaveProperty('lastPriceTs')
-      expect(response).toHaveProperty('closePriceTs')
-      expect(response).toHaveProperty('orderbookTs')
-      expect(response).toHaveProperty('instrumentUid')
     })
   })
 })
