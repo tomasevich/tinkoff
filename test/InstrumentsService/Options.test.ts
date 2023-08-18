7f1b6f124881e46196fb259ee4b9fa48e9f44c0f
Vyacheslav <vyacheslav.tomasevich@gmail.com>
2023-08-18 07:45:09 +0000

    добавлены тесты на получение инструм-ов по статусу

---
+++ test/InstrumentsService/Options.test.ts
@@ -0,0 +1,35 @@
+import dotenv from 'dotenv'
+
+import { InstrumentStatus, InstrumentsService } from '../../src'
+
+dotenv.config({ path: './.env.test' })
+
+const TOKEN = process.env.TINKOFF_INVEST_API_TOKEN ?? ''
+const instrumentsService = new InstrumentsService(TOKEN, true)
+
+describe('instrumentStatus as INSTRUMENT_STATUS_BASE', () => {
+  test('Expect "response" to have property "instruments"', async () => {
+    const response = await instrumentsService.Options({
+      instrumentStatus: InstrumentStatus.INSTRUMENT_STATUS_BASE
+    })
+    expect(response).toHaveProperty('instruments')
+  })
+})
+
+describe('instrumentStatus as INSTRUMENT_STATUS_ALL', () => {
+  test('Expect "response" to have property "instruments"', async () => {
+    const response = await instrumentsService.Options({
+      instrumentStatus: InstrumentStatus.INSTRUMENT_STATUS_ALL
+    })
+    expect(response).toHaveProperty('instruments')
+  })
+})
+
+describe('instrumentStatus as INSTRUMENT_STATUS_UNSPECIFIED', () => {
+  test('Expect "response" to have property "instruments"', async () => {
+    const response = await instrumentsService.Options({
+      instrumentStatus: InstrumentStatus.INSTRUMENT_STATUS_UNSPECIFIED
+    })
+    expect(response).toHaveProperty('instruments')
+  })
+})
