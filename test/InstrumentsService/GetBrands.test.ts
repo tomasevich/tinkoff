dcf21e83ee4eea7015c9d489f73f207eeac74e1f
Vyacheslav <vyacheslav.tomasevich@gmail.com>
2023-08-18 14:37:12 +0000

    обновлено описание теста

--- test/InstrumentsService/GetBrands.test.ts
+++ test/InstrumentsService/GetBrands.test.ts
@@ -7,6 +7,6 @@ dotenv.config({ path: './.env.test' })
 // const TOKEN = process.env.TINKOFF_INVEST_API_TOKEN ?? ''
 // const instrumentsService = new InstrumentsService(TOKEN, true)
 
-describe('Empty request', () => {
+describe('Empty body', () => {
   test.todo('Expect "response" to have property "brands"')
 })
