e047b50aa379f6f941422d0e1008a1a60e4264f9
Vyacheslav <vyacheslav.tomasevich@gmail.com>
2023-08-18 07:33:21 +0000

    добавлены команды на отдельные тесты

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   transform: { '^.+\\.ts?$': 'ts-jest' },
   testEnvironment: 'node',
-  testRegex: '/test/InstrumentsService/Bonds.test.ts',
-  // testRegex: '/test/.*\\.(test|spec)?\\.(ts)$',
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
   verbose: true
 }
