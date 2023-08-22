bb67ba4220f4c5cd14941d8ba5da907a026c891f
Vyacheslav <vyacheslav.tomasevich@gmail.com>
2023-08-22 10:14:02 +0000

    пропущены тесты открытия и закрытия счетов

--- test/SandboxService/OpenSandboxAccount.test.ts
+++ test/SandboxService/OpenSandboxAccount.test.ts
@@ -8,7 +8,7 @@ const TOKEN = process.env.TINKOFF_INVEST_API_TOKEN ?? ''
 const sandboxService = new SandboxService(TOKEN, true)
 
 describe('Empty body', () => {
-  test('Expect "response" to have property "{}"', async () => {
+  test.skip('Expect "response" to have property "{}"', async () => {
     const response = await sandboxService.OpenSandboxAccount({})
     expect(response).toEqual({})
   })
