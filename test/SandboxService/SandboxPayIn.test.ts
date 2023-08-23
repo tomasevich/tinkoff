83ccab539afc3f68c125cb3706ab027ab9f82e38
Vyacheslav <vyacheslav.tomasevich@gmail.com>
2023-08-23 11:45:12 +0300

    добавлен тест пополнения баланса

--- test/SandboxService/SandboxPayIn.test.ts
+++ test/SandboxService/SandboxPayIn.test.ts
@@ -1,3 +1,20 @@
-describe('Empty body', () => {
-  test.todo('Expect "response" to have property "???"')
+import dotenv from 'dotenv'
+
+import { SandboxService } from '../../src'
+
+dotenv.config({ path: './.env.test' })
+
+const TOKEN = process.env.TINKOFF_INVEST_API_TOKEN ?? ''
+const ACCOUNT_ID = process.env.TINKOFF_INVEST_API_ACCOUNT_ID ?? ''
+const sandboxService = new SandboxService(TOKEN, true)
+
+describe('Пополняем баланс', () => {
+  test('Должен вернуть текущий баланс', async () => {
+    const response = await sandboxService.SandboxPayIn({
+      accountId: ACCOUNT_ID,
+      amount: SandboxService.StringToMoneyValue('114,25 rub')
+    })
+
+    expect(response).toHaveProperty('balance')
+  })
 })
