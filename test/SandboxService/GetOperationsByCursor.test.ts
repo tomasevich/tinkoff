b28d1b8f77405739c32453e27f33f039f5291866
Vyacheslav <vyacheslav.tomasevich@gmail.com>
2023-08-23 11:51:42 +0300

    обновлен тест получения операций с пагинацией

--- test/SandboxService/GetOperationsByCursor.test.ts
+++ test/SandboxService/GetOperationsByCursor.test.ts
@@ -8,9 +8,14 @@ const TOKEN = process.env.TINKOFF_INVEST_API_TOKEN ?? ''
 const ACCOUNT_ID = process.env.TINKOFF_INVEST_API_ACCOUNT_ID ?? ''
 const sandboxService = new SandboxService(TOKEN, true)
 
-describe('accountId as ACCOUNT_ID', () => {
-  describe('state as OPERATION_STATE_CANCELED', () => {
-    test('Expect "response" to have property "operations"', async () => {
+describe.each([
+  OperationState.OPERATION_STATE_CANCELED,
+  OperationState.OPERATION_STATE_EXECUTED,
+  OperationState.OPERATION_STATE_PROGRESS,
+  OperationState.OPERATION_STATE_UNSPECIFIED
+])('Получаем операции с пагинацией', operationState => {
+  describe('Статус операции %i', () => {
+    test('Должен вернуть список операций с пагинацией', async () => {
       const response = await sandboxService.GetSandboxOperationsByCursor({
         accountId: ACCOUNT_ID,
         instrumentId: '6afa6f80-03a7-4d83-9cf0-c19d7d021f76',
@@ -18,79 +23,7 @@ describe('accountId as ACCOUNT_ID', () => {
         to: '2023-08-19T00:00:00:000Z',
         cursor: '',
         limit: 10,
-        state: OperationState.OPERATION_STATE_CANCELED,
-        operationTypes: [
-          OperationType.OPERATION_TYPE_BUY,
-          OperationType.OPERATION_TYPE_SELL
-        ],
-        withoutCommissions: false,
-        withoutTrades: false,
-        withoutOvernights: false
-      })
-      expect(response).toHaveProperty('hasNext')
-      expect(response).toHaveProperty('nextCursor')
-      expect(response).toHaveProperty('items')
-    })
-  })
-
-  describe('state as OPERATION_STATE_EXECUTED', () => {
-    test('Expect "response" to have property "operations"', async () => {
-      const response = await sandboxService.GetSandboxOperationsByCursor({
-        accountId: ACCOUNT_ID,
-        instrumentId: '6afa6f80-03a7-4d83-9cf0-c19d7d021f76',
-        from: '2023-07-18T00:00:00:000Z',
-        to: '2023-08-19T00:00:00:000Z',
-        cursor: '',
-        limit: 10,
-        state: OperationState.OPERATION_STATE_EXECUTED,
-        operationTypes: [
-          OperationType.OPERATION_TYPE_BUY,
-          OperationType.OPERATION_TYPE_SELL
-        ],
-        withoutCommissions: false,
-        withoutTrades: false,
-        withoutOvernights: false
-      })
-      expect(response).toHaveProperty('hasNext')
-      expect(response).toHaveProperty('nextCursor')
-      expect(response).toHaveProperty('items')
-    })
-  })
-
-  describe('state as OPERATION_STATE_PROGRESS', () => {
-    test('Expect "response" to have property "operations"', async () => {
-      const response = await sandboxService.GetSandboxOperationsByCursor({
-        accountId: ACCOUNT_ID,
-        instrumentId: '6afa6f80-03a7-4d83-9cf0-c19d7d021f76',
-        from: '2023-07-18T00:00:00:000Z',
-        to: '2023-08-19T00:00:00:000Z',
-        cursor: '',
-        limit: 10,
-        state: OperationState.OPERATION_STATE_PROGRESS,
-        operationTypes: [
-          OperationType.OPERATION_TYPE_BUY,
-          OperationType.OPERATION_TYPE_SELL
-        ],
-        withoutCommissions: false,
-        withoutTrades: false,
-        withoutOvernights: false
-      })
-      expect(response).toHaveProperty('hasNext')
-      expect(response).toHaveProperty('nextCursor')
-      expect(response).toHaveProperty('items')
-    })
-  })
-
-  describe('state as OPERATION_STATE_UNSPECIFIED', () => {
-    test('Expect "response" to have property "operations"', async () => {
-      const response = await sandboxService.GetSandboxOperationsByCursor({
-        accountId: ACCOUNT_ID,
-        instrumentId: '6afa6f80-03a7-4d83-9cf0-c19d7d021f76',
-        from: '2023-07-18T00:00:00:000Z',
-        to: '2023-08-19T00:00:00:000Z',
-        cursor: '',
-        limit: 10,
-        state: OperationState.OPERATION_STATE_UNSPECIFIED,
+        state: operationState,
         operationTypes: [
           OperationType.OPERATION_TYPE_BUY,
           OperationType.OPERATION_TYPE_SELL
